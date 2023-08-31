691d426a7a4a699a641690419797665eee7e2511
Your Name <you@example.com>
2023-08-31 13:27:36 +0700

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,35 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import Root from "./routes/root.jsx";
+import ErrorPage from "./error-page";
+import Contact from "./routes/contact";
+import Surat from "./routes/surat"
+import Surat1 from "./routes/surat1";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "contacts/:contactId",
+    element: <Contact />,
+  },
+  {
+    path: "surat/:suratId",
+    element: <Surat />,
+  },
+  {
+    path: "surat1/:suratId",
+    element: <Surat1 />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
